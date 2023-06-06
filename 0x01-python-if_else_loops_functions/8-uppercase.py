cb77efad21eeff0f83959931eb3113cd25ca1551
Kerubel Solomon <kibi729@gmail.com>
2023-06-06 09:52:11 -0700

    po

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,7 +3,7 @@
 # Kerubel Solomon <kibi729@gmail.com>
 
 def uppercase(str):
-    
+    """Print a string in uppercase."""
     for c in str:
         if ord(c) >= 97 and ord(c) <= 122:
             c = chr(ord(c) - 32)
