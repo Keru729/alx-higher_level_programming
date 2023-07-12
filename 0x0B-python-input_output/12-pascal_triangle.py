681cb98304d5618f6459936fba789392252a326b
Kerubel Solomon <kibi729@gmail.com>
2023-07-11 19:57:42 -0700

    pl

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -8,14 +8,14 @@ def pascal_triangle(n):
     Pascal’s triangle of n
     """
 
-    if m <= 0:
+    if n <= 0:
         return []
 
     triangle = [[1]]
 
-    for m in range(1, m):
+    for x in range(1, n):
         row = [1]
-        for y in range(1, m):
+        for y in range(1, x):
             row.append(triangle[x-1][y-1] + triangle[x-1][y])
         row.append(1)
         triangle.append(row)
